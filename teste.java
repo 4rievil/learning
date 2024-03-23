2e770d648cd514b7b7cbfdcab44ee7d5cb80dc3f
Isabella Nunes <149917017+4rievil@users.noreply.github.com>
2024-03-23 20:57:30 -0300

    tentando

---
+++ teste.java
@@ -0,0 +1,27 @@
+nome01
+nome02
+nome03
+normal
+ascegas
+ambos
+
+se {
+
+nome01 seleciona normal
+então: filtro normal
+
+}
+
+elseif {
+
+nome02 seleciona ascegas
+então: filtro ascegas
+
+}
+
+elseif {
+
+nome03 seleciona ambos
+então:
+
+ve
